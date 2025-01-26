145df93b88ec36e034edccf8a8ebba883442b652
Divy611 <divy.parikh@hotmail.com>
2025-01-26 22:10:20 +0530

    Profile details added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fintech_app/onboarding/onboarding.dart';
+import 'package:fintech_app/profile/profile.dart';
 
 void main() {
   runApp(const FintechApp());
@@ -9,6 +9,10 @@ class FintechApp extends StatelessWidget {
   const FintechApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: OnboardingPage());
-  }
+    return MaterialApp(
+      title: 'Fintech App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: ProfilePage()),
+    );
+  } //TODO: Change to onboarding
 }
